1c40860fe918596cfc6be1e574e182e60194b5b6
briley <brian.riley@ucop.edu>
2014-10-24 11:32:56 -0700

    Code cleanup

--- director.js
+++ director.js
@@ -1,22 +1,18 @@
-var _ = require('underscore'),
-    fs = require('fs'),
-		http = require('http'),
-		url = require('url');
-		
+require('./lib/init');
+
 var Stagehand = require('./lib/stagehand'),
     Writer = require('./lib/writer'),
     Critic = require('./lib/critic'),
-    Actor = require('./lib/model/actor'),
     Play = require('./lib/model/play'),
-		Script = require('./lib/script');
+    Script = require('./lib/script');
     
 var firstTime = true,
     play_count = 1,
     plays_completed = 0,
     stagehand = undefined,
-		writer = undefined,
-		critic = undefined;
-		
+    writer = undefined,
+    critic = undefined;
+    
 // Wait for all of the plays to complete!
 var waitUntilDone = setInterval(function(){
   
@@ -27,74 +23,72 @@ var waitUntilDone = setInterval(function(){
       // Get the Actors ready
       console.log('\nHolding auditions.');
       
-			// Assign a Script Writer
-	    writer = new Writer(function(success){
-				
-				stagehand = new Stagehand(function(success){
-				
-					if(success){
-						var fatal = false;
-				
-						// Wait for systems to come online
-						console.log('Waiting a moment to give all systems a chance to come online');
-						setTimeout(function(){
-						
-							writer.setStagehand(stagehand);
-						
-							writer.findAllActors(function(actors){
-				        // Load plays
-				        writer.findAllPlays(function(plays){
-									plays_count = plays.length;
+      // Assign a Script Writer
+      writer = new Writer(function(success){
+        
+        stagehand = new Stagehand(function(success){
+        
+          if(success){
+            var fatal = false;
+        
+            // Wait for systems to come online
+            console.log('Waiting a moment to give all systems a chance to come online');
+            setTimeout(function(){
+            
+              writer.setStagehand(stagehand);
+            
+              writer.findAllActors(function(actors){
+                // Load plays
+                writer.findAllPlays(function(plays){
+                  plays_count = plays.length;
 
-									// Wait for the servers to startup and come online
-				          _.forEach(plays, function(play){
-										// Assign a Critic to review the play
-			              var critic = new Critic(play.getId());
-										
-										var script = new Script(writer, stagehand, play);
+                  // Wait for the servers to startup and come online
+                  _.forEach(plays, function(play){
+                    // Assign a Critic to review the play
+                    var critic = new Critic(play);
+                    
+                    var script = new Script(writer, stagehand, play);
       
 // THIS IS COMMENTED OUT SO WE CAN RUN AGAINST LIVE FOR THIS PHASE!
-										//script.dressRehearsal(critic, actors, function(){
-			
-										script.audition(critic, actors, function(){
-											
-											critic.writeReview(function(res){
-												console.log(res);
-												plays_completed++;
-											});
-										});
-									});
-				
-								});
-							});
-				
-						}, 4000);
-				
-					}else{
-						console.log('A fatal error prevented the rehearsal from starting!');
-					}
-						
-				});
-				
-			});
-			
+                    //script.dressRehearsal(critic, actors, function(){
+      
+                    script.audition(critic, actors, function(){
+                      
+                      critic.writeReview(function(res){
+                        console.log(res);
+                        plays_completed++;
+                      });
+                    });
+                  });
+        
+                });
+              });
+        
+            }, 4000);
+        
+          }else{
+            console.log('A fatal error prevented the rehearsal from starting!');
+          }
+            
+        });
+        
+      });
+      
       firstTime = false;
       
     });
   }
 
   if(plays_completed >= play_count){
-console.log('tic waitUntilDone');
-		
     clearInterval(waitUntilDone);
     
-		if(writer instanceof(Writer)) writer.close();
+    if(writer instanceof(Writer)) writer.close();
     if(stagehand instanceof(Stagehand)) stagehand.cleanup();
   }
 
 }, 400);
 
-
+// -----------------------------------------------------------------------------------------
 var deleteFolderRecursive = function(path, callback) {
   if(fs.existsSync(path)){
     
